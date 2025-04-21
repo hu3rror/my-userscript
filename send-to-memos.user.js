3eebfcdccf1d6140f67db344d930beee3cb8759f
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2025-04-21 15:19:45 +0800

    更新发送到 Memos 用户脚本的命名空间和版本号，提升至2.5

--- send-to-memos.user.js
+++ send-to-memos.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name            发送到 Memos
 // @name:en         Send to Memos
-// @namespace       http://tampermonkey.net/
-// @version         2.4
+// @namespace       https://github.com/hu3rror/my-userscript
+// @version         2.5
 // @description     将选中的链接或文本发送到 Memos，支持配置、添加源页面信息和快速发送按钮
 // @description:en  Send selected links or text to Memos, supports configuration, adding source page information, and quick send buttons
 // @author          Hu3rror
